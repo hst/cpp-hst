61c5ad4e9e56bf106a993d389e988f9b56352c80
Douglas Creager <dcreager@google.com>
2017-03-02 07:14:30 -0500

    Add traces behavior
    This is the first semantic model that we want to support.  Each semantic
    model will have its own wrapper struct, containing all of the classes
    and methods that define that method.  That will let us parameterize
    certain bits of the refinement checker in a nicely generic way.
    
    In the traces model, the behavior of a process is the set of non-τ
    events that it can perform.  That's easy enough to calculate, just by
    calling the process's `initials` method.

---
+++ src/hst/semantic-models.h
@@ -0,0 +1,37 @@
+/* -*- coding: utf-8 -*-
+ * -----------------------------------------------------------------------------
+ * Copyright © 2017, HST Project.
+ * Please see the COPYING file in this distribution for license details.
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef HST_SEMANTIC_MODELS_H
+#define HST_SEMANTIC_MODELS_H
+
+#include "hst/event.h"
+#include "hst/process.h"
+
+namespace hst {
+
+#if 0
+// Each semantic model is defined by a struct that must implement the following
+// signature:
+struct SemanticModel {
+    class Behavior;
+    static Behavior get_process_behavior(const Process& process);
+    static Behavior get_process_behavior(const Process::Set& processes);
+};
+#endif
+
+struct Traces {
+    // In the traces model, the behavior of a process is the set of non-τ events
+    // that it can perform.
+    using Behavior = Event::Set;
+
+    static Behavior get_process_behavior(const Process& process);
+    static Behavior get_process_behavior(const Process::Set& processes);
+};
+
+}  // namespace hst
+
+#endif  // HST_SEMANTIC_MODELS_H
